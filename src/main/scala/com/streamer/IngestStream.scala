09e60240839679c439afc7b004ce712bd37810c5
surajatreyac <suraj.atreya@glassbeam.com>
2015-05-31 18:14:22 +0530

    Made autoflush to true

--- src/main/scala/com/streamer/IngestStream.scala
+++ src/main/scala/com/streamer/IngestStream.scala
@@ -5,13 +5,22 @@ import java.io.DataOutputStream
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
-
-import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.HttpsURLConnection
+import java.net.ServerSocket
+import java.io.PrintWriter
 
 object IngestStream {
   
   val userAgent = "Mozilla/5.0";
   val url = "http://stream.meetup.com/2/rsvps"
+  
+  val serverSocket = new ServerSocket(5555)
+  val clientSocket = serverSocket.accept
+  
+  def sendMessage(msg: String) = {
+    val out = new PrintWriter(clientSocket.getOutputStream, true)
+    out.println(msg)
+  }
   
   def sendGet() = {
     val obj = new URL(url)
@@ -22,18 +31,17 @@ object IngestStream {
     con.setRequestProperty("User-Agent", userAgent)
     
     val responseCode = con.getResponseCode
-    
+   
     val in =  new BufferedReader(
             new InputStreamReader(con.getInputStream()))
     
     while(true){
       val inputLine = in.readLine()
-      println(inputLine)
+      sendMessage(inputLine)
     }
   }
   
   def main(args: Array[String]): Unit = {
-    println("hello")
     sendGet()
   }
 }
